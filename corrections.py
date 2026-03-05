7dabdc0cb632b0529039ebf2889991494b6da745
shadow99-web <rajendrasingh2320@gmail.com>
2026-03-06 00:37:56 +0530

    Create pokemon_map for name corrections
    Added a mapping of incorrect Pokémon names to their correct forms.

---
+++ corrections.py
@@ -0,0 +1,16 @@
+# corrections.py
+
+pokemon_map = {
+    "BATTLECYCLIZAR": "BATTLE CYCLIZAR",
+    "SOLOSS": "SOLOSIS",
+    "STUN": "STUNKY",
+    "ARONp": "ARON",
+    "HOOTH": "HO-OH",
+    "HONEDCE": "HONEDGE",
+    "NACL": "NACLI",
+    "CLAMPER": "CLAMPERL",
+    "eoe": "OSHAWOTT",
+    "rYNAMO": "TYNAMO",
+    "PICNITE": "PIGNITE",
+    "DUCTRIO": "DUGTRIO"
+}
