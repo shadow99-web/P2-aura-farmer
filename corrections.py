b39402c81bc60878b59ff91572aa35cb7e0d138c
shadow99-web <rajendrasingh2320@gmail.com>
2026-03-07 19:44:22 +0530

    Correct Pokémon names in pokemon_map
    Updated various Pokémon names in the pokemon_map dictionary for accuracy.

--- corrections.py
+++ corrections.py
@@ -4,14 +4,61 @@ pokemon_map = {
     "BATTLECYCLIZAR": "BATTLE CYCLIZAR",
     "SOLOSS": "SOLOSIS",
     "STUN": "STUNKY",
-    "ARONp": "ARON",
-    "HOOTH": "HO-OH",
+    "ARONP": "ARON",
+    "HOOTH": "HOHO",
     "HONEDCE": "HONEDGE",
     "NACL": "NACLI",
-    "CLAMPER": "CLAMPERL",
-    "eoe": "OSHAWOTT",
-    "rYNAMO": "TYNAMO",
+    "CLAMPER": "PERLU",
+    "EOE": "OTTARO",
+    "RYNAMO": "TYNAMO",
     "PICNITE": "PIGNITE",
-    "DUCTRIO": "DUGTRIO",
-    "CALARIANPONYTA":"GALARIAN PONYTA"
+    "KINCAMBIT": "KINGAMBIT",
+    "NOONE": "LINOONE",
+    "GLICAR": "GLIGAR",
+    "MACNEMITE": "COIL",
+    "TOCEPI": "TOGEPI",
+    "SPIKYEAREDPICHU": "PICHU",
+    "STANLY": "STARLY",
+    "APPUN": "APPLIN",
+    "DRIZZLE": "DRIZZILE",
+    "FIDOUCH": "FIDOUGH",
+    "LOKI": "LOKIX",
+    "NAO": "NATU",
+    "ONI": "ONIX",
+    "CRESPIN": "CHESPIN",
+    "COLBAT": "GOLBAT",
+    "BRONZONC": "BRONZONG",
+    "WHITEPLUMAGESQUAWKABILLY": "SQUAWKABILLY",
+    "KANCASKHAN": "GARURA",
+    "CROWLE": "GROVYLE"
 }
+
+pokemon_map["MACBY"] = " BUBY"
+pokemon_map["MACNEZONE"] = " JIBACOIL"
+pokemon_map["BLUEPLUMAGESQUAWKABILLY"] = " SQUAWKABILLY"
+pokemon_map["ROAMINGGIMMICHOUL"] = " GIMMIGHOUL"
+pokemon_map["NIDORANM"] = " NIDORAN"
+pokemon_map["DROOPYTATSUCIRI"] = " TATSUGIRI"
+pokemon_map["REWOR"] = " RELLOR"
+pokemon_map["FLAMICO"] = "FLAMIGO"
+pokemon_map["JANGMOO"] = " MINIRAS"
+pokemon_map["ARMAROUCE"] = " ARMAROUGE"
+pokemon_map["CASTLY"] = " GHOS"
+pokemon_map["FLETCHLINC"] = "→ DARTIRI"
+pokemon_map["HAKAMOO"] = " MEDIRAS"
+pokemon_map["NOLANEXECCUTOR"] = " EXEGGUTOR"
+pokemon_map["WHITESTRIPEDBASCULIN"] = " BASCULIN"
+pokemon_map["NOLANSANDSHREW"] = " SANDSHREW"
+pokemon_map["BRAMBLECHAST"] = "BRAMBLEGHAST"
+pokemon_map["WADDLE"] = "SEWADDLE"
+pokemon_map["SHELCON"] = "SHELGON"
+pokemon_map["SUCCOO"] = "SLIGGOO"
+pokemon_map["GIMMICHOUL"] = " GIMMIGHOUL"
+pokemon_map["DICLETT"] = " DIGDA"
+pokemon_map["CREAVARD"] = " GREAVARD"
+pokemon_map["BLUEFLOWERFLABEBE"] = " FLABEBE"
+pokemon_map["EXECCCUTE"] = "EXEGGCUTE"
+pokemon_map["CHARJABUC"] = "AKKUP"
+pokemon_map["TINKATU"] = "TINKATUFF"
+pokemon_map["MACCARCO"] = "MAGCARGO"
+pokemon_map["OINKOLOCNE"] = "OINKOLOGNE"
